6fd38c6640ee998a4c5f63bd0372ed0fb07fa2ff
Slavasil <subvia@proton.me>
2025-03-09 14:37:04 +0300

    create WorkoutActivity and WorkoutCountdownFragment

--- app/src/main/java/com/example/trainsmart/ui/workouts/WorkoutDetailsFragment.kt
+++ app/src/main/java/com/example/trainsmart/ui/workouts/WorkoutDetailsFragment.kt
@@ -1,15 +1,18 @@
 package com.example.trainsmart.ui.workouts
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.trainsmart.R
+import com.example.trainsmart.WorkoutActivity
 
 class WorkoutsDetailsFragment : Fragment() {
 
@@ -34,6 +37,7 @@ class WorkoutsDetailsFragment : Fragment() {
         val workoutCountExersices: TextView = view.findViewById(R.id.tv_exercise_count_exercises)
         val workoutImage: ImageView = view.findViewById(R.id.iv_exercise)
         val rV: RecyclerView = view.findViewById(R.id.rv_exercises)
+        val startButton: Button = view.findViewById(R.id.btn_start)
 
         workout?.let{
             workoutTitle.text = it.title
@@ -42,6 +46,10 @@ class WorkoutsDetailsFragment : Fragment() {
             workoutImage.setImageResource(it.photo)
             rV.layoutManager = LinearLayoutManager(requireContext())
             rV.adapter = ExerciseAdapter(it.exercises)
+        }
+
+        startButton.setOnClickListener {
+            startActivity(Intent(context, WorkoutActivity::class.java))
         }
 
         return view
