4db32152e9538e35b2ba349464b36052e4f54458
Michael <87703210+FbGod@users.noreply.github.com>
2025-03-21 15:55:42 +0300

    exercises page firestore functionality

--- app/src/main/java/com/example/trainsmart/firestore/FireStoreClient.kt
+++ app/src/main/java/com/example/trainsmart/firestore/FireStoreClient.kt
@@ -1,5 +1,6 @@
 package com.example.trainsmart.firestore
 
+import com.example.trainsmart.data.Exercise
 import com.example.trainsmart.data.User
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.CoroutineScope
@@ -14,14 +15,18 @@ class FireStoreClient {
     private val tag = "FirestoreClient: "
 
     private val db = FirebaseFirestore.getInstance()
-    private val collection = "users"
+    private val collectionUsers = "users"
+    private val collectionBasicEx = "basic-exercises"
 
+    /**
+     *   /users/ collection interface
+     */
 
     fun insertUser(
         user: User
     ): Flow<String?> {
         return callbackFlow {
-            db.collection(collection)
+            db.collection(collectionUsers)
                 .document(user.id)
                 .set(user.ToHashMap())
                 .addOnSuccessListener { document ->
@@ -45,7 +50,7 @@ class FireStoreClient {
         user: User
     ): Flow<Boolean> {
         return callbackFlow {
-            db.collection(collection)
+            db.collection(collectionUsers)
                 .document(user.id)
                 .set(user.ToHashMap())
                 .addOnSuccessListener { document ->
@@ -65,7 +70,7 @@ class FireStoreClient {
         id: String
     ): Flow<User?> {
         return callbackFlow {
-            db.collection(collection)
+            db.collection(collectionUsers)
                 .get()
                 .addOnSuccessListener { result ->
 
@@ -106,6 +111,94 @@ class FireStoreClient {
             id = this["id"] as String,
             email = this["email"] as String,
             username = this["username"] as String
+        )
+    }
+
+    /** /basic-exercises/ interface
+     *  only get() methods. It's is a built-in collection.
+     */
+
+    fun getExercise(
+        name: String
+    ): Flow<Exercise?> {
+        return callbackFlow {
+            db.collection(collectionBasicEx)
+                .get()
+                .addOnSuccessListener { result ->
+
+                    var exercise: Exercise? = null
+                    for (document in result) {
+                        if (document.data["name"] == name) {
+
+                            exercise = document.data.toExercise()
+                            println(tag + "found exercise with id: ${exercise.name}")
+                            trySend(exercise)
+                        }
+
+                        if (exercise == null) {
+                            println(tag + "exercise not found: $name")
+                            trySend(null)
+                        }
+                    }
+                }
+                .addOnFailureListener { e ->
+                    e.printStackTrace()
+                    println(tag + "error getting exercise: ${e.message}")
+                    trySend(null)
+                }
+            awaitClose{}
+        }
+    }
+
+    fun getAllExercises(
+    ): Flow<List<Exercise?>> {
+        return callbackFlow {
+            db.collection(collectionBasicEx)
+                .get()
+                .addOnSuccessListener { result ->
+
+                    var exercises = mutableListOf<Exercise>()
+
+                    for (document in result) {
+
+                        val exercise = document.data.toExercise()
+                        exercises.add(exercise);
+
+                    }
+
+                    if (exercises.isEmpty()) {
+                        println(tag + "exercises list is empty")
+                        trySend(emptyList())
+                    }
+                    else {
+                        println(tag + "exercises list not null")
+                        trySend(exercises)
+                    }
+                }
+                .addOnFailureListener { e ->
+                    e.printStackTrace()
+                    println(tag + "error getting exercises: ${e.message}")
+                    trySend(emptyList())
+                }
+            awaitClose{}
+        }
+    }
+
+    private fun Exercise.ToHashMap(): HashMap<String, Any> {
+        return hashMapOf(
+            "name" to name,
+            "photoUrl" to photoUrl,
+            "description" to description,
+            "technique" to technique
+        )
+    }
+
+    private fun Map<String, Any>.toExercise(): Exercise {
+        return Exercise(
+            name = this["name"] as String,
+            photoUrl = this["photoUrl"] as String,
+            description = this["description"] as String,
+            technique = this["technique"] as String
         )
     }
 
