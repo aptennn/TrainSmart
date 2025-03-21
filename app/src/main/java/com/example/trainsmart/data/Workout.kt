87c2322e60085cf1accf6ce2d06494e8d2dcf1ec
Michael <87703210+FbGod@users.noreply.github.com>
2025-03-21 19:06:30 +0300

    some methods and changes in firestore client [not tested yet]

--- app/src/main/java/com/example/trainsmart/data/Workout.kt
+++ app/src/main/java/com/example/trainsmart/data/Workout.kt
@@ -4,7 +4,7 @@ data class Workout(
     val name : String = "Name",
     val photoUrl : String = "Url",
     val duration : String = "Duration",
-    val exercisesIds: Map<String, String> = mapOf("uid" to "times-repetitions"), // подходы-повторения
+    val exercises: Map<String, String> = mapOf("uid" to "times-repetitions"), // подходы-повторения
     val description : String = "Description",
     val type : String = "Type"
 )
--- app/src/main/java/com/example/trainsmart/firestore/FireStoreClient.kt
+++ app/src/main/java/com/example/trainsmart/firestore/FireStoreClient.kt
@@ -2,6 +2,7 @@ package com.example.trainsmart.firestore
 
 import com.example.trainsmart.data.Exercise
 import com.example.trainsmart.data.User
+import com.example.trainsmart.data.Workout
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -17,6 +18,7 @@ class FireStoreClient {
     private val db = FirebaseFirestore.getInstance()
     private val collectionUsers = "users"
     private val collectionBasicEx = "basic-exercises"
+    private val collectionBasicWorkouts = "basic-workouts"
 
     /**
      *   /users/ collection interface
@@ -118,32 +120,31 @@ class FireStoreClient {
      *  only get() methods. It's is a built-in collection.
      */
 
-    fun getExercise(
-        name: String
-    ): Flow<Exercise?> {
+    fun getExercisesByIds(
+        idS: List<String>
+    ): Flow<List<Exercise>?> {
         return callbackFlow {
             db.collection(collectionBasicEx)
                 .get()
                 .addOnSuccessListener { result ->
 
-                    var exercise: Exercise? = null
+                    val exercises = mutableListOf<Exercise>()
                     for (document in result) {
-                        if (document.data["name"] == name) {
+                        if (idS.contains(document.id)) {
 
-                            exercise = document.data.toExercise()
-                            println(tag + "found exercise with id: ${exercise.name}")
-                            trySend(exercise)
-                        }
-
-                        if (exercise == null) {
-                            println(tag + "exercise not found: $name")
-                            trySend(null)
+                            val exercise = document.data.toExercise()
+                            exercises.add(exercise)
+                            println(tag + "found exercise with id: ${document.id}")
                         }
                     }
+                    if (exercises.size == 0)
+                        trySend(null)
+                    else
+                        trySend(exercises)
                 }
                 .addOnFailureListener { e ->
                     e.printStackTrace()
-                    println(tag + "error getting exercise: ${e.message}")
+                    println(tag + "error getting exercises: ${e.message}")
                     trySend(null)
                 }
             awaitClose{}
@@ -201,5 +202,67 @@ class FireStoreClient {
             technique = this["technique"] as String
         )
     }
+
+    /** /basic-workouts/ interface
+     *  only get() methods. It's is a built-in collection.
+     */
+
+
+    fun getAllWorkouts(
+    ): Flow<List<Workout?>> {
+        return callbackFlow {
+            db.collection(collectionBasicWorkouts)
+                .get()
+                .addOnSuccessListener { result ->
+
+                    val workouts = mutableListOf<Workout>()
+
+                    for (document in result) {
+
+                        val workout = document.data.toWorkout()
+                        workouts.add(workout);
+
+                    }
+
+                    if (workouts.isEmpty()) {
+                        println(tag + "workouts list is empty")
+                        trySend(emptyList())
+                    }
+                    else {
+                        println(tag + "workouts list not null")
+                        trySend(workouts)
+                    }
+                }
+                .addOnFailureListener { e ->
+                    e.printStackTrace()
+                    println(tag + "error getting workouts: ${e.message}")
+                    trySend(emptyList())
+                }
+            awaitClose{}
+        }
+    }
+
+    private fun Workout.ToHashMap(): HashMap<String, Any> {
+        return hashMapOf(
+            "name" to name,
+            "photoUrl" to photoUrl,
+            "duration" to duration,
+            "description" to description,
+            "exercises" to exercises,
+            "type" to type
+        )
+    }
+
+    private fun Map<String, Any>.toWorkout(): Workout {
+        return Workout(
+            name = this["name"] as String,
+            photoUrl = this["photoUrl"] as String,
+            duration = this["type"] as String,
+            description = this["description"] as String,
+            exercises = this["exercises"] as Map<String, String>,
+            type = this["type"] as String
+        )
+    }
+
 
 }
