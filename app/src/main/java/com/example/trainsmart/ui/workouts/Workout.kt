3a289d632c0d17b9fe4c88425c78991e9d243d34
aptennn <uegor@protonmail.com>
2025-03-25 19:58:23 +0300

    Update Workout.kt

--- app/src/main/java/com/example/trainsmart/ui/workouts/Workout.kt
+++ app/src/main/java/com/example/trainsmart/ui/workouts/Workout.kt
@@ -3,6 +3,7 @@ package com.example.trainsmart.ui.workouts
 import android.os.Parcelable
 import com.example.trainsmart.ui.exercises.ExerciseListItemModel
 import kotlinx.parcelize.Parcelize
+import java.util.Objects
 
 @Parcelize
 data class Workout(val title: String, val photo: Int, val time: String, val exercises: List<ExerciseListItemModel>, val type: Int) : Parcelable {
