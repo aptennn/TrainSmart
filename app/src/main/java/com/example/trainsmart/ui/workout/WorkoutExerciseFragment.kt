b088296d9d77bcf509b7fbc531bacffe456871ad
Slavasil <subvia@proton.me>
2025-03-27 19:47:18 +0300

    fix WorkoutActivity crashing because of misinterpretation of sets+reps strings

--- app/src/main/java/com/example/trainsmart/ui/workout/WorkoutExerciseFragment.kt
+++ app/src/main/java/com/example/trainsmart/ui/workout/WorkoutExerciseFragment.kt
@@ -33,7 +33,7 @@ class WorkoutExerciseFragment : Fragment() {
             setIndex = it.getInt(ARGNAME_SET_INDEX)
         }
         exercise = workout?.exercises?.get(exerciseIndex!!)
-        nSets = parseNumSets(exercise?.countReps!!)
+        nSets = extractNumSets(exercise?.countReps!!)
     }
 
     override fun onCreateView(
@@ -47,7 +47,7 @@ class WorkoutExerciseFragment : Fragment() {
         val imageView: ImageView = root.findViewById(R.id.currentExercisePicture)
         imageView.setImageResource(exercise.photo)
         val repetitionCountTextView: TextView = root.findViewById(R.id.currentExerciseRepetitionCount)
-        repetitionCountTextView.text = parseNumReps(exercise.countReps).toString()
+        repetitionCountTextView.text = extractNumReps(exercise.countReps).toString()
 
         val buttonStart: Button = root.findViewById(R.id.buttonStart)
         buttonStart.setOnClickListener { (activity as WorkoutActivity).onNextSetClicked() }
@@ -55,20 +55,20 @@ class WorkoutExerciseFragment : Fragment() {
         val progressBar: WorkoutProgressBar = root.findViewById(R.id.workoutProgress)
         progressBar.currentExercise = exerciseIndex!!
         progressBar.currentSet = setIndex!!
-        progressBar.setCounts = workout!!.exercises.map { parseNumSets(it.countReps) }.toTypedArray()
+        progressBar.setCounts = workout!!.exercises.map { extractNumSets(it.countReps) }.toTypedArray()
         return root
     }
 
-    private fun parseNumReps(s: String): Int {
-        val words = s.split(' ')
-        if (words.size != 5)
+    private fun extractNumReps(s: String): Int {
+        val words = s.split('-')
+        if (words.size != 2)
             throw IllegalArgumentException("invalid sets+reps string")
-        return words[3].toInt()
+        return words[1].toInt()
     }
 
-    private fun parseNumSets(s: String): Int {
-        val words = s.split(' ')
-        if (words.size != 5)
+    private fun extractNumSets(s: String): Int {
+        val words = s.split('-')
+        if (words.size != 2)
             throw IllegalArgumentException("invalid sets+reps string")
         return words[0].toInt()
     }
